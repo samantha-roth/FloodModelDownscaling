1096870735227bfcebbf272a2f7c89fd7624dc62
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2023-10-16 23:40:22 -0400

    Update makeDimSimObs30.R

--- dataProcessing/ST/makeDimSimObs30.R
+++ dataProcessing/ST/makeDimSimObs30.R
@@ -15,15 +15,15 @@ obsWE_RS<- obsWE_OR_5m[RS]
 
 save(obsWE_RS,RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/obsWE_RS.RData")
 
-load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_10m.RData")
-load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_30m.RData")
-load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_50m.RData")
+#load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_10m.RData")
+#load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_30m.RData")
+#load("C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/5m_closest_50m.RData")
 
-closest10m_RS<- hwm_closest.10m[RS,]
-closest30m_RS<- hwm_closest.30m[RS,]
-closest50m_RS<- hwm_closest.50m[RS,]
+#closest10m_RS<- hwm_closest.10m[RS,]
+#closest30m_RS<- hwm_closest.30m[RS,]
+#closest50m_RS<- hwm_closest.50m[RS,]
 
-save(closest10m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest10m_RS.RData")
-save(closest30m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest30m_RS.RData")
-save(closest50m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest50m_RS.RData")
+#save(closest10m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest10m_RS.RData")
+#save(closest30m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest30m_RS.RData")
+#save(closest50m_RS,file="C:/Users/svr5482/Reification/Philly/data/Norristown/nCh/closest50m_RS.RData")
 
